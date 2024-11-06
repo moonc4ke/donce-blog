c32345e901c3ab06b225fac99940e62f13cdee52
mooncake <adom.donatas@gmail.com>
2024-11-06 20:32:09 +0200

    fix: delete image bug by using custom buttons

---
+++ app/javascript/controllers/delete_button_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    url: String,
+    confirm: String
+  }
+
+  async confirmAndDelete(event) {
+    event.preventDefault()
+
+    if (confirm(this.confirmValue)) {
+      const response = await fetch(this.urlValue, {
+        method: 'DELETE',
+        headers: {
+          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+        },
+        credentials: 'same-origin'
+      })
+
+      if (response.ok) {
+        const frame = this.element.closest('turbo-frame')
+        if (frame) {
+          frame.remove()
+        }
+      }
+    }
+  }
+}    
