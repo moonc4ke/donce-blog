1c4b1c913cf9911880e956ba2b37ade5897c71c4
mooncake <adom.donatas@gmail.com>
2024-11-30 15:44:18 +0200

    feat: add Blog Post creation seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 100.times do |i|
+#   BlogPost.create title: "Blog Post #{i}", short_body: "Short Body #{i}", body: "Main Body of Blog Post #{i}"
+# end
