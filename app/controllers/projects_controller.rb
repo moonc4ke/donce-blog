c1b517ac03777cfac81e04d0a3ca758c67ce1f4f
mooncake <adom.donatas@gmail.com>
2024-12-18 20:49:47 +0200

    allow unauthenticated users to view projects index view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  allow_unauthenticated_access(only: [ :index ])
+
   def index
     # For now, we'll leave this empty until we have a Project model
     # or want to add static project data
