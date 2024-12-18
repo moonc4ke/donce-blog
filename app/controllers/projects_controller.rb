3e6febb6d65acac65215135618255fbbb802fc94
mooncake <adom.donatas@gmail.com>
2024-12-18 20:35:14 +0200

    add initial logic and structure for projects page

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ApplicationController
+  def index
+    # For now, we'll leave this empty until we have a Project model
+    # or want to add static project data
+  end
+end
