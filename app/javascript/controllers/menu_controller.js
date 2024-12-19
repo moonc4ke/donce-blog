b3de5053f493b67a6e16f168110b7a09745052dd
mooncake <adom.donatas@gmail.com>
2024-12-19 11:33:08 +0200

    fix menu overlay display

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["button", "menu", "links"]
+  static outlets = ["main-content", "footer"]
 
   connect() {
     this.closeMenu()
@@ -19,6 +20,8 @@ export default class extends Controller {
     this.buttonTarget.textContent = "ESC"
     this.menuTarget.classList.add("nav-menu--open")
     this.linksTarget.classList.add("nav-menu__links--open")
+    if (this.hasMainContentOutlet) this.mainContentOutletElement.style.display = "none"
+    if (this.hasFooterOutlet) this.footerOutletElement.style.display = "none"
     document.body.style.overflow = "hidden"
   }
 
@@ -26,6 +29,8 @@ export default class extends Controller {
     this.buttonTarget.textContent = "MENU"
     this.menuTarget.classList.remove("nav-menu--open")
     this.linksTarget.classList.remove("nav-menu__links--open")
+    if (this.hasMainContentOutlet) this.mainContentOutletElement.style.display = "block"
+    if (this.hasFooterOutlet) this.footerOutletElement.style.display = "block"
     document.body.style.overflow = ""
   }
 }
