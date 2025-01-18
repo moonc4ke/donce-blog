ed56d3bd6e51546f6bcae21c8af2d3c78a86e48b
Donatas Adomavicius <adom.donatas@gmail.com>
2025-01-18 18:01:46 +0100

    try to preload fonts for production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.version = "1.1"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# Explicitly precompile font files
-Rails.application.config.assets.precompile += %w[
-  InterVariable.woff2
-  InterVariable-Italic.woff2
-  fonts.css
-]
