83394c30752d406e0decea991b2cf5edfb866af0
Donatas Adomavicius <donce@donce.dev>
2025-04-19 18:12:06 +0100

    Remove credentials.rb initializer for production key override

--- config/initializers/credentials.rb
+++
@@ -1,4 +0,0 @@
-# Override credentials key with production key from environment if available
-if Rails.env.production? && ENV["RAILS_PRODUCTION_KEY"].present?
-  Rails.application.credentials.key = ENV["RAILS_PRODUCTION_KEY"]
-end
